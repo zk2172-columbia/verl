9b09838cfceee48dcac970c2c32e834d65ff2785
cz2678 <cz2678@columbia.edu>
2025-08-28 15:19:33 -0400

    Add math demo

--- verl/utils/reward_score/__init__.py
+++ verl/utils/reward_score/__init__.py
@@ -45,7 +45,8 @@ def default_compute_score(
         from . import gsm8k
 
         res = gsm8k.compute_score(solution_str, ground_truth)
-    elif data_source in ["lighteval/MATH", "DigitalLearningGmbH/MATH-lighteval", "HuggingFaceH4/MATH-500"]:
+    # elif data_source in ["lighteval/MATH", "DigitalLearningGmbH/MATH-lighteval", "HuggingFaceH4/MATH-500"]:
+    elif data_source.startswith("DigitalLearningGmbH/MATH-lighteval"):
         from . import math
 
         res = math.compute_score(solution_str, ground_truth)
